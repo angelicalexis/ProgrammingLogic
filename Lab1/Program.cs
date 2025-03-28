8a763352e715a8e702f9f443d1f31f7e65a4d249
angelicalexis <a.alexishall@gmail.com>
2025-03-28 02:36:46 +0000

    Added Lab1

---
+++ Lab1/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+  class Program
+  {
+    static void Main(string[] args)
+    {
+      // Step 1
+      Console.WriteLine("Angelica IT-1050-Programming Logic");
+
+      // Step 2
+      /*
+      Name: Angelica
+      Title: IT - 1050 - Lab 1
+      */
+
+      // Step 3
+      int num = 7;
+      string lang = "Python";
+      double progs = 2.50;
+      bool expert = true;
+
+      Console.WriteLine("My favorite number is " + num + ". My favorite programming language is " + lang + ". I have written " + progs + " programs before this. And, it is " + expert + " that I have some programming experience");
+
+      // Step 5
+      const string school = "Cuyahoga Community College";
+      Console.WriteLine("My school name is " + school + ".");
+    }
+  }
